a7e6883ff6bea0a1d566af312e6d498288102c5d
Ron Grimes <ron.grimes@pobox.com>
2021-02-20 15:36:58 -0500

    Added "choose" command... with: up, down,define,remove. To come: select, password ... then conversion into paxnews proper.

--- wifilib.py
+++ wifilib.py
@@ -25,6 +25,8 @@ class Wifi:
 #    Returns False python if a key is not pressed/released.
 #
 
+    key_val ={ "up", "down", "define", "select", "remove", "password", "exit" }
+
     def __init__(self):
         pass
         
@@ -52,20 +54,19 @@ class Wifi:
         return names
 
     def list_networks(self):
-        names = {}
+        self.networks = {}
         result = self.wpa_cli("list_networks")
         lines = result.splitlines()[1:]   # separate lines, discard 1st line
         for line in lines:
             ll = line.split()
             if ll[1] == "any":
                 ll[1] = ""
-            names[ll[1]] = int(ll[0])
-        return names
+            self.networks[ll[1]] = int(ll[0])
 
-    def add_network(self):
+    def add_network(self, nname=""):
         try:
             print("Enter values, ^C to exit.")
-            name =     input("New network name: ")
+            name = input("New network name: ") if nname == "" else nname
             password = input("Network password: ")
         except KeyboardInterrupt:
             return
@@ -77,16 +78,19 @@ class Wifi:
         result = self.wpa_cli("save_config")
         return result
 
-    def remove_network(self):
-        try:
-            print("Enter value, ^C to exit.")
-            id = input("Network to remove: ")
-            id = int(id)
-        except KeyboardInterrupt:
-            return
-        except ValueError:
-            print("(ignored)")
-            return
+    def remove_network(self, ssid=""):
+        if ssid == "":
+            try:
+                print("Enter value, ^C to exit.")
+                id = input("Network # to remove: ")
+                id = int(id)
+            except KeyboardInterrupt:
+                return
+            except ValueError:
+                print("(ignored)")
+                return
+        else:
+            id = self.networks[ssid]
         result = self.wpa_cli("remove_network", id)
         discard = self.wpa_cli("save_config")
         return result
@@ -138,27 +142,69 @@ class Wifi:
             return 2
         return 1
 
+    def build_line(self,d):
+        return d["current_ap"] + d["locked"] + d["strength"] + d["defined"] + " " + d["ssid"]  
+
     def build_scroll(self):
         status = wifi.status()
-        networks = wifi.list_networks()
+        wifi.list_networks()
         ssids = wifi.scan_results()
-        scroll = []
+        wifi.scroll = []
         for ssid in ssids:
             current_ap = "*" if "ssid" in status and \
                                 status["ssid"] == ssid else " "
             locked = wifi.is_locked(ssids[ssid][1])
             strength = str(wifi.sig_strength(ssids[ssid][0]))
-            defined = "D" if ssid in networks else " " 
-            scroll.append((current_ap, locked, strength, defined, ssid))
-        return scroll
+            defined = "D" if ssid in wifi.networks else " "
+            ssid_info ={"current_ap":current_ap, "locked":locked, "strength":strength,
+                        "defined":defined, "ssid":ssid}
+            ssid_info["line"] = self.build_line(ssid_info)
+            wifi.scroll.append(ssid_info)
+
+    def getkey(self):
+        kbd = True
+        if kbd:
+            return readchar.readchar()
+        else:
+            # get input from keypad > TBD
+            pass
+        return key
 
     def choose(self):
-        print(" key>", flush=True, end="")
-#       print(repr(readchar.readkey()))
-        try:
-            print(readchar.readchar())
-        except termios.error:
-            print()
+        s_line = 0 
+        
+        while True:
+            print(wifi.scroll[s_line]["line"] + " ", end="", flush=True)
+            key  = wifi.getkey()
+
+            if   key in ("u", "1"):  # Up 
+                print()
+                s_line += 1
+                if s_line >= len(wifi.scroll):
+                    s_line = 0
+            elif key in ("d", "4", "\r", " "):  # Down
+                print()
+                s_line = s_line - 1 if s_line > 0 else len(wifi.scroll) - 1
+            elif key in ("f", "2"):  # deFine
+                print("f")
+                wifi.add_network(wifi.scroll[s_line]["ssid"])
+                wifi.scroll[s_line]["defined"] = "D"
+                wifi.scroll[s_line]["line"] = self.build_line(wifi.scroll[s_line])
+            elif key in ("s", "3"):  # Select
+                print("s")
+            elif key in ("r", "6"):  # Remove
+                print("r")
+                wifi.remove_network(wifi.scroll[s_line]["ssid"])
+                wifi.scroll[s_line]["defined"] = " "
+                wifi.scroll[s_line]["line"] = self.build_line(wifi.scroll[s_line])
+            elif key in ("p", "5"):  # Password
+                print("p")
+            elif key in ("q", "x", "*"):  # Quit, eXit
+                print()
+                return
+            else:
+                print()
+
 
 '''
         # get my accesspoint name
@@ -178,24 +224,25 @@ if (__name__ == "__main__"):
 #           keypad.wait_keypress()
 #           print(keypad.key,  keypad.cseconds, keypad.keytype)
             cli = input('scan, list_n, add, remove, select, status, ' +
-                        'reconfigure, all, scroll*: ').strip()
-
-            if cli == "scan"[0:len(cli)]:
+                        'reconfigure, all, choose*, quit: ').strip()
+            if len(cli) == 0:
+                continue
+            elif cli == "scan"[0:len(cli)]:
                 ssids = wifi.scan_results()
                 print(ssids)
             elif cli == "list_n"[0:len(cli)]:
-                networks = wifi.list_networks()
-                print(networks)
+                wifi.list_networks()
+                print(wifi.networks)
             elif cli == "add"[0:len(cli)]:
                 result = wifi.add_network()
                 print(result)
-                networks = wifi.list_networks()
-                print(networks)
+                wifi.list_networks()
+                print(wifi.networks)
             elif cli == "remove"[0:len(cli)]:
                 result = wifi.remove_network()
                 print(result)
-                networks = wifi.list_networks()
-                print(networks)
+                wifi.list_networks()
+                print(wifi.networks)
             elif cli == "select"[0:len(cli)]:
                 result = wifi.select_network()
                 print(result)
@@ -207,16 +254,17 @@ if (__name__ == "__main__"):
             elif cli == "reconfigure"[0:len(cli)]:
                 result = wifi.reconfigure()
                 print(result)
-                networks = wifi.list_networks()
-                print(networks)
+                wifi.list_networks()
+                print(wifi.networks)
             elif cli == "all"[0:len(cli)]:
-                scroll = wifi.build_scroll()
-                for line in scroll:
-#                        (current_ap, locked, strength, defined, ssid)
-                    print(line[0]+line[1]+line[2]+line[3], line[4])
-            elif cli == "scroll"[0:len(cli)]:
-                scroll = wifi.build_scroll()
+                wifi.build_scroll()
+                for line in wifi.scroll:
+                    print(line["line"])
+            elif cli == "choose"[0:len(cli)]:
+                wifi.build_scroll()
                 wifi.choose()
+            elif cli == "quit"[0:len(cli)]:
+                break
             
     except KeyboardInterrupt:
         print()
